5740e91fef681a2107a4b11daf3a4dc4185a7325
ivan.s.rivera@gmail.com <ivan.s.rivera@gmail.com>
2021-04-01 14:00:51 +0200

    added a script for api testing

---
+++ scripts/mock_requests.sh
@@ -0,0 +1,29 @@
+# mock_requests.sh
+# This script is meant to be used interactively -- run it and use the functions it provides
+# make sure that your backend is running
+
+BACKEND=http://localhost:4000
+
+# needs an email and a password
+function create_user {
+  http POST $BACKEND/api/v1/registration \
+  user:="{\"email\": \"$1\", \"password\": \"$2\", \"password_confirmation\": \"$2\"}"
+}
+
+# needs an email and a password
+function log_in {
+  http POST $BACKEND/api/v1/session \
+  user:="{ \"email\": \"$1\", \"password\": \"$2\" }"
+}
+
+# needs access token
+function log_out {
+  http DELETE $BACKEND/api/v1/session \
+  "Authorization: $1"
+}
+
+# needs refresh token
+function renew_session {
+  http POST $BACKEND/api/v1/session/renew \
+  "Authorization: $1"
+}
